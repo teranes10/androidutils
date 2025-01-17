59237cf6c11b50d262a5f68dfaf071af645ba7de
Teranes <ranjithteranes10@gmail.com>
2025-01-17 16:15:04 +0530

    1.0.9

--- app/src/main/java/com/github/teranes10/androidutils/utils/VersionUpdateUtil.java
+++ app/src/main/java/com/github/teranes10/androidutils/utils/VersionUpdateUtil.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.content.FileProvider;
@@ -13,35 +14,21 @@ import java.io.File;
 public class VersionUpdateUtil {
     private static final String TAG = "VersionUpdateUtil";
 
-    public static void installAPK(Activity context, String filePath)  {
-        File file = new File(filePath);
+    public static void installAPK(Activity context, Uri uri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
-        if (file.exists()) {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uriFromFile(context, new File(filePath)), "application/vnd.android.package-archive");
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        Thread thread = new Thread(() -> {
+            try {
+                context.startActivity(intent);
+            } catch (Exception e) {
+                Log.e(TAG, "installAPK: ", e);
+            }
+        });
 
-
-            // startActivity(intent);
-            Thread thread = new Thread(() -> {
-                try {
-                    context.startActivity(intent);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            });
-            thread.start();
-            context.finish();
-
-        } else {
-            Toast.makeText(context, "Installing another version of myDevice.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private static Uri uriFromFile(Context context, File file) {
-        return FileProvider.getUriForFile(context, "au.com.softclient.mydevices.provider", file);
+        thread.start();
+        context.finish();
     }
 }
