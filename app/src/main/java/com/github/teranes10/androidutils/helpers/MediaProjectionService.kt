12756981c252663fbc3dcd53acee0898d7585457
Teranes <ranjithteranes10@gmail.com>
2025-05-08 05:43:04 +0530

    1.5.8

--- app/src/main/java/com/github/teranes10/androidutils/helpers/MediaProjectionService.kt
+++ app/src/main/java/com/github/teranes10/androidutils/helpers/MediaProjectionService.kt
@@ -12,6 +12,9 @@ import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -22,9 +25,13 @@ import com.github.teranes10.androidutils.utils.PermissionUtil
 import java.io.File
 
 abstract class MediaProjectionService : ForegroundService() {
-
-    protected var intent: Intent? = null
-    protected var resultCode: Int? = null
+    protected val handler = Handler(Looper.getMainLooper())
+    protected var mediaProjectionManager: MediaProjectionManager? = null; private set
+    protected var mediaProjection: MediaProjection? = null; private set
+    protected var mediaRecorder: MediaRecorder? = null; private set
+    protected var virtualDisplay: VirtualDisplay? = null; private set
+    protected var intent: Intent? = null; private set
+    protected var resultCode: Int? = null; private set
     protected abstract fun onStartService(context: Context, resultCode: Int, intent: Intent)
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -33,10 +40,11 @@ abstract class MediaProjectionService : ForegroundService() {
 
     override fun onCommand(intent: Intent, flags: Int, startId: Int): Int? {
         if (intent.action == ACTION_START_FOREGROUND_SERVICE) {
+            this.mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
             this.resultCode = intent.getIntExtra("resultCode", RESULT_CANCELED)
             this.intent = intent.parcelable<Intent>("data")
 
-            if (this.resultCode != RESULT_OK || this.intent == null) {
+            if (this.mediaProjectionManager == null || this.resultCode != RESULT_OK || this.intent == null) {
                 return START_NOT_STICKY
             }
         }
@@ -52,25 +60,32 @@ abstract class MediaProjectionService : ForegroundService() {
         stopRecording()
     }
 
-    private var mediaProjection: MediaProjection? = null
-    private var mediaRecorder: MediaRecorder? = null
-    private var virtualDisplay: VirtualDisplay? = null
-
     private fun createMediaRecorder(): MediaRecorder {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             MediaRecorder(this)
         } else {
-            @Suppress("DEPRECATION") MediaRecorder()
+            @Suppress("DEPRECATION") (MediaRecorder())
         }
     }
 
     fun startRecording(file: File, width: Int = 1280, height: Int = 720, bitrate: Int = 5_000_000, frameRate: Int = 30) {
         if (resultCode != RESULT_OK || intent == null) {
+            Log.e(TAG, "startRecording: result code or intent is not valid")
             return
         }
 
-        val mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode!!, intent!!)
+        mediaProjection = mediaProjectionManager?.getMediaProjection(resultCode!!, intent!!)
+        if (mediaProjection == null) {
+            Log.e(TAG, "startRecording: media projection is null")
+            return
+        }
+
+        mediaProjection?.registerCallback(object : MediaProjection.Callback() {
+            override fun onStop() {
+                super.onStop()
+                stopRecording()
+            }
+        }, handler)
 
         mediaRecorder = createMediaRecorder().apply {
             setVideoSource(MediaRecorder.VideoSource.SURFACE)
