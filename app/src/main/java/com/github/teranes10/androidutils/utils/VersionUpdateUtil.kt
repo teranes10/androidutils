9622906f63ec767a8241684a7ca5e3cfff5a57a5
Teranes <ranjithteranes10@gmail.com>
2025-05-22 18:30:41 +0530

    1.6.1

--- app/src/main/java/com/github/teranes10/androidutils/utils/VersionUpdateUtil.kt
+++ app/src/main/java/com/github/teranes10/androidutils/utils/VersionUpdateUtil.kt
@@ -1,34 +1,45 @@
-package com.github.teranes10.androidutils.utils;
+package com.github.teranes10.androidutils.utils
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
-import android.widget.Toast;
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import com.github.teranes10.androidutils.extensions.ContextExtensions.getUriForFile
+import java.io.File
 
-import androidx.core.content.FileProvider;
+object VersionUpdateUtil {
+    private const val TAG = "VersionUpdateUtil"
 
-import java.io.File;
+    fun Context.openUnknownSourcesSettings() {
+        val intent = Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES).apply {
+            data = Uri.parse("package:$packageName")
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
+    }
 
-public class VersionUpdateUtil {
-    private static final String TAG = "VersionUpdateUtil";
+    fun Context.installAPK(apkFile: File) {
+        if (!packageManager.canRequestPackageInstalls()) {
+            openUnknownSourcesSettings()
+            return
+        }
 
-    public static void installAPK(Activity context, Uri uri) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(uri, "application/vnd.android.package-archive");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        val uri = getUriForFile(apkFile)
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(uri, "application/vnd.android.package-archive")
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
 
-        Thread thread = new Thread(() -> {
-            try {
-                context.startActivity(intent);
-            } catch (Exception e) {
-                Log.e(TAG, "installAPK: ", e);
-            }
-        });
-
-        thread.start();
-        context.finish();
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e(TAG, "No Activity found to handle APK installation.", e)
+            Toast.makeText(this, "Unable to install APK", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Log.e(TAG, "Unexpected error installing APK", e)
+        }
     }
 }
